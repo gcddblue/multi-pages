26fbd023ade5e2650c35bf7cddc783019865269a
高晨辉 <gaochenhui@xinnet.com>
2019-08-29 14:19:17 +0800

    多页配置

---
+++ src/pages/page2/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <p>
+      我是page2<a href="/index.html" style="color:red;margin-left:20px;"
+        >返回首页</a
+      >
+    </p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style></style>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const glob = require('glob')
+
+function getEntry(url) {
+  let entrys = {}
+  glob.sync(url).forEach(item => {
+    let urlArr = item.split('/').splice(-3)
+    entrys[urlArr[1]] = {
+      entry: 'src/pages/' + urlArr[1] + '/' + 'index.js',
+      template: 'src/pages/' + urlArr[1] + '/' + 'index.html',
+      filename: urlArr[1] + '.html',
+      title: 'pages-' + urlArr[1]
+    }
+  })
+  return entrys
+}
+let pages = getEntry('./src/pages/**?/*.html')
+module.exports = {
+  pages
+}
